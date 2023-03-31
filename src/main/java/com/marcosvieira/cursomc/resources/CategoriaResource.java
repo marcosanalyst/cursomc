195fb9237544ee3e040ede1e3d04ef1c85a711a4
Marcos Vieira <analyst.marcos@gmail.com>
2023-03-31 20:08:01 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/marcosvieira/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcosvieira/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.marcosvieira.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.marcosvieira.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -11,8 +16,16 @@ public class CategoriaResource {
 
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String Listar() {
-		return "Rest está funcionando!";
+	public List<Categoria> Listar() {
+		
+		Categoria  cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 		
 	}
 }
