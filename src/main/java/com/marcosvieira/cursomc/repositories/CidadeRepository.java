6a17831f8a4f4ad5f8043f79585466f6570e4f32
Marcos Vieira <analyst.marcos@gmail.com>
2023-04-11 20:32:51 -0300

    EStado e Cidade

---
+++ src/main/java/com/marcosvieira/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,14 @@
+package com.marcosvieira.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcosvieira.cursomc.domain.Cidade;
+
+@Repository
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
+	
+
+}
